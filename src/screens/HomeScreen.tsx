659c8c88573e9066818f000f61f0be01fc1093f2
Büşra Arıgün <busrarigun@gmail.com>
2023-05-16 22:54:59 +0300

    new package added

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,26 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text} from 'react-native';
+import React from 'react';
 
 const HomeScreen = () => {
+  const chartConfig = {
+    backgroundGradientFrom: "#1E2923",
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: "#08130D",
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    strokeWidth: 2, // optional, default 3
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false // optional
+  };
+  const data = {
+    labels: ['Swim', 'Bike', 'Run'], // optional
+    data: [0.4, 0.6, 0.8],
+  };
   return (
     <View>
-      <Text>HomeScreen</Text>
+     
     </View>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
